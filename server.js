835a1ebbd3e245b8f486b4820fdcd478b7720001
KevinGodek <92218951+KevinGodek@users.noreply.github.com>
2022-03-20 19:55:15 -0400

    refactored code

--- server.js
+++ server.js
@@ -18,11 +18,12 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./Develop/routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
